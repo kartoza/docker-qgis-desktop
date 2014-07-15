3d79fdb1d8fa118e6c43c092e09d28f1989440d4
Tim Sutton <tim@linfiniti.com>
2014-07-15 15:01:35 +0200

    Added gtkrc dire to volumes

--- run-qgis-in-docker.sh
+++ run-qgis-in-docker.sh
@@ -1,8 +1,12 @@
 docker.io kill qgis-desktop
 docker.io rm qgis-desktop
 xhost +
+# Config so QGIS finds your Qt and QGIS settings
+# Gtkrc so Qt finds your Gtk settings
+# Users home is mounted as home
 docker.io run --name="qgis-desktop" \
 	-v ${HOME}/.config:/.config \
+        -v ${HOME}/.gtkrc-2.0:/.gtkrc-2.0 \
 	-v ${HOME}:/home/${USER} \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=unix$DISPLAY \
