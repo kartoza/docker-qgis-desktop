f66bd25fb899665a550a4829da9e10055cdefe5f
Tim Sutton <tim@linfiniti.com>
2014-09-10 14:06:04 +0200

    Updates to readme

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 docker build -t kartoza/qgis-desktop:latest .
-mkdir -p $HOME/bin
-echo "# Next line added for running QGIS in docker" >> ${HOME}/.bashrc
-echo "export PATH=\${PATH}:\${HOME}/bin" >> ${HOME}/.bashrc
-cp run-qgis-in-docker.sh ${HOME}/bin
-chmod +x ${HOME}/bin/run-qgis-in-docker.sh
-cp QGIS-2.4.Docker.desktop ${HOME}/.local/share/applications/
+sudo cp run-qgis-in-docker.sh /usr/local/bin
+sudo chmod +x ${HOME}/bin/run-qgis-in-docker.sh
+sudo cp QGIS-2.4.Docker.desktop /usr/share/applications/
+sudo cp qgis-icon-60x60.png /usr/local
 
 echo "A shortcut was created to run your docker - "
 echo "you may need to reload your desktop"
