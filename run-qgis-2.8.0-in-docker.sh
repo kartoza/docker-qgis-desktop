bb129122c4084e9c94f27b3781af355596c49b89
Tim Sutton <tim@kartoza.com>
2015-02-21 08:47:03 +0200

    Fixed desktop launcher for 2.8

--- run-qgis-2.8.0-in-docker.sh
+++ run-qgis-2.8.0-in-docker.sh
@@ -5,5 +5,5 @@ docker run --rm --name="qgis-desktop-2-8-0" \
 	-v ${HOME}:/home/${USER} \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=unix$DISPLAY \
-	kartoza/qgis-desktop:latest 
+	kartoza/qgis-desktop:2.8.0
 xhost -
