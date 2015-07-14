68801c23b3e79e58a4113cb02ca57bec667ec355
Etienne Trimaille <etienne.trimaille@gmail.com>
2015-07-14 14:00:23 +0200

    update to 2.8.2

--- run-qgis-2.8.2-in-docker.sh
+++ run-qgis-2.8.2-in-docker.sh
@@ -5,5 +5,5 @@ docker run --rm --name="qgis-desktop-2-8-2" \
 	-v ${HOME}:/home/${USER} \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=unix$DISPLAY \
-	kartoza/qgis-desktop:2.8.0
+	kartoza/qgis-desktop:2.8.2
 xhost -
