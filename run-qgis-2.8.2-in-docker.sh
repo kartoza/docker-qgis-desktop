375ff2278737118bdf429617ddfe703e94793f5c
Etienne Trimaille <etienne.trimaille@gmail.com>
2015-07-14 14:00:19 +0200

    update to 2.8.2

--- run-qgis-2.8.2-in-docker.sh
+++ run-qgis-2.8.2-in-docker.sh
@@ -1,6 +1,6 @@
 xhost +
 # --rm will remove the container as soon as it ends
-docker run --rm --name="qgis-desktop-2-8-0" \
+docker run --rm --name="qgis-desktop-2-8-2" \
 	-i -t \
 	-v ${HOME}:/home/${USER} \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
