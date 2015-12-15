67ad7004dc7cd2d352842677755391cf2444abb8
Etienne Trimaille <etienne.trimaille@gmail.com>
2015-12-15 12:28:14 +0100

    update to 2.8.3 and fix readme

--- 2.8/run-qgis-2.8.4-in-docker.sh
+++ 2.8/run-qgis-2.8.4-in-docker.sh
@@ -6,10 +6,10 @@ xhost +
 #	-v ${HOME}/.config:/.config \
 #	-v ${HOME}/.gtkrc-2.0:/.gtkrc-2.0 \
 
-docker run --rm --name="qgis-desktop-2.8.3 \
+docker run --rm --name="qgis-desktop-2.8.4 \
 	-i -t \
 	-v ${HOME}:/home/${USER} \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=unix$DISPLAY \
-	kartoza/qgis-desktop:2.8.3
+	kartoza/qgis-desktop:2.8.4
 xhost -
