d10a8f603e036d8b92da132d70680447ee2f6c58
Conor Anderson <conor.anderson@mail.utoronto.ca>
2016-11-10 13:26:17 -0500

    Cleanup version issues

--- 2.14/build/clean.sh
+++ 2.14/build/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-apt-get remove -y --purge krb5-multidev build-essential bison cmake make cpp cpp-4.9 gfortran-5 g++ g++-4.9 gcc gcc-4.9 \
+apt-get remove -y --purge krb5-multidev build-essential bison cmake make cpp cpp-5 gfortran-5 g++ gcc gcc-5 \
     flex libclang1-3.5 git qt4-qmake rsync cmake-data g++-5 git-man liberror-perl qt4-linguist-tools libqt4-dev-bin
 dpkg --purge `dpkg -l "*-dev" | sed -ne 's/ii  \(.*-dev\(:amd64\)\?\) .*/\1/p'` || true
 
