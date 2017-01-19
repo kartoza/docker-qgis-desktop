d77d29e4fded34cc2bebd1ae7d1f5dc339cabe55
Conor Anderson <ConorIA@users.noreply.github.com>
2017-01-18 21:24:25 -0500

    Replace `start.sh` and bump to 2.14.8 (#22) (#23)
    * Fix startscript and bump
    
    * DESKTOP=ON
    
    * missing space
    
    * Cleanup version issues

--- 2.14/build/make.sh
+++ 2.14/build/make.sh
@@ -10,15 +10,15 @@ cmake /build/QGIS \
     -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so \
     -DQSCINTILLA_INCLUDE_DIR=/usr/include/qt4 \
     -DQWT_LIBRARY=/usr/lib/libqwt.so \
-    -DWITH_DESKTOP=OFF \
-    -DWITH_SERVER=ON \
+    -DWITH_DESKTOP=ON \
+    -DWITH_SERVER=OFF \
     -DBUILD_TESTING=OFF \
     -DWITH_INTERNAL_QWTPOLAR=ON
 
 make install -j8
 ldconfig
 
-strip /usr/bin/qgis_mapserv.fcgi
+#strip /usr/bin/qgis_mapserv.fcgi
 strip `find /usr/lib/ -name "libqgis*" -type f`
 strip `find  /usr/share/qgis/ -name "*.so" -type f`
 
