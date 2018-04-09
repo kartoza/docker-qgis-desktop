2b73b08b52eaa69994cd2616941862dbf9fffe22
Xavi Torelló <info@xaviertorello.cat>
2018-04-09 12:49:41 +0200

    Avoid install recommended packages and integrate pyqt4 qsci dev

--- 2.18/build/getDeps.sh
+++ 2.18/build/getDeps.sh
@@ -8,14 +8,14 @@ apt-get -y update
 # #-------------Application Specific Stuff ----------------------------------------------------
 # removed: doxygen cmake-curses-gui
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get update &&  \
-    apt-get install -y git cmake flex bison libproj-dev libgeos-dev libgdal1-dev \
+    apt-get install --no-install-recommends-y git cmake flex bison libproj-dev libgeos-dev libgdal1-dev \
     libexpat1-dev libfcgi-dev libgsl0-dev libpq-dev libqca2-dev libqca2-plugin-ossl \
     libqscintilla2-dev libqt4-dev libqt4-opengl-dev libqt4-sql-sqlite \
     libqtwebkit-dev libqwt-dev libspatialindex-dev libspatialite-dev libsqlite3-dev \
     lighttpd locales pkg-config poppler-utils pyqt4-dev-tools python python-dev \
     python-qt4 python-qt4-dev python-qt4-sql python-sip python-sip-dev \
     spawn-fcgi xauth xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xvfb \
-    python-pip gdal-bin python-gdal
+    python-pip gdal-bin python-gdal pyqt4.qsci-dev
 
 pip install requests psycopg2 osgeo
 
