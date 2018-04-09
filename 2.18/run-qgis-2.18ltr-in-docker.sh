609cb41d52991d0b76fa2ed3e81fad99f20ffb76
Xavi Torelló <info@xaviertorello.cat>
2018-04-09 13:42:48 +0200

    Share host network to 2.18 container

--- 2.18/run-qgis-2.18ltr-in-docker.sh
+++ 2.18/run-qgis-2.18ltr-in-docker.sh
@@ -4,5 +4,6 @@ docker run --rm --name="qgis-desktop-ltr" \
 	-v ${HOME}:/home/${USER} \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=unix$DISPLAY \
+	--net=host \
 	kartoza/qgis-desktop:LTR
 xhost -
