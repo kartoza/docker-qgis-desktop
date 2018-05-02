142aa32f9f68b5779e10a025ab3afad65a3ea4a2
Samweli <smwltwesa6@gmail.com>
2018-05-02 19:47:20 +0300

    reverted master docker config

--- master/build/getCode.sh
+++ master/build/getCode.sh
@@ -4,3 +4,4 @@ set -e
 cd /build
 git clone --depth 1 -b master git://github.com/qgis/QGIS.git
 cd QGIS
+git checkout final-2_14_6
--- master/build/getDeps.sh
+++ master/build/getDeps.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 set -e
 
@@ -6,16 +7,13 @@ apt-get -y update
 # #-------------Application Specific Stuff ----------------------------------------------------
 # removed: doxygen cmake-curses-gui
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get update &&  \
-    apt-get install -y git cmake flex bison libproj-dev libgeos-dev\
+    apt-get install -y git cmake flex bison libproj-dev libgeos-dev libgdal1-dev \
     libexpat1-dev libfcgi-dev libgsl0-dev libpq-dev libqca2-dev libqca2-plugin-ossl \
     libqscintilla2-dev libqt4-dev libqt4-opengl-dev libqt4-sql-sqlite \
     libqtwebkit-dev libqwt-dev libspatialindex-dev libspatialite-dev libsqlite3-dev \
     lighttpd locales pkg-config poppler-utils pyqt4-dev-tools python python-dev \
     python-qt4 python-qt4-dev python-qt4-sql python-sip python-sip-dev \
-    spawn-fcgi xauth xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xvfb \
-    software-properties-common python-software-properties && \
-    add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable && apt-get update && apt-get upgrade && \
-    apt-get install gdal-bin python-gdal python3-gdal
+    spawn-fcgi xauth xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xvfb 
 
 chmod -R a+w /usr/lib/x86_64-linux-gnu/qt4/plugins/designer/
 chmod -R a+w /usr/lib/python2.7/dist-packages/PyQt4/uic/widget-plugins/
