a904b7bd367f5207c8dd4a6347ba74fec2933139
Xavi Torelló <info@xaviertorello.cat>
2018-05-17 16:12:22 +0200

    2.18 improvements (#32)
    * Add local plugins mounting to docker image
    
    * Add pip installation + requests and osgeo python libs
    
    * Solve gdal requirement
    
    * Solve psycopg2 requirement
    
    * Avoid install recommended packages and integrate pyqt4 qsci dev
    
    * Add jinja2 dependency
    
    * Add python-qscintilla2 dependency
    
    * Add pygments dependency
    
    * Add python-pyspatialite lib
    
    * Devel plugins are handled via home mount
    
    * Share host network to 2.18 container
    
    * Fix 2.18 version at README
    
    * Migrate to apt and simplify dependencies solving
    
    Use apt tool instead of apt-get and simplify updates
    
    * Add python-setuptools

--- 2.18/build/getDeps.sh
+++ 2.18/build/getDeps.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 set -e
-apt-get -y update
-apt-get install -y software-properties-common
+apt -y update
+apt install -y software-properties-common
 add-apt-repository ppa:ubuntugis/ubuntugis-unstable
-apt-get -y update
+apt -y update
 
 # #-------------Application Specific Stuff ----------------------------------------------------
 # removed: doxygen cmake-curses-gui
-LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get update &&  \
-    apt-get install -y git cmake flex bison libproj-dev libgeos-dev libgdal1-dev \
+LC_ALL=C DEBIAN_FRONTEND=noninteractive apt install --no-install-recommends -y \
+    git cmake flex bison libproj-dev libgeos-dev libgdal1-dev \
     libexpat1-dev libfcgi-dev libgsl0-dev libpq-dev libqca2-dev libqca2-plugin-ossl \
     libqscintilla2-dev libqt4-dev libqt4-opengl-dev libqt4-sql-sqlite \
     libqtwebkit-dev libqwt-dev libspatialindex-dev libspatialite-dev libsqlite3-dev \
     lighttpd locales pkg-config poppler-utils pyqt4-dev-tools python python-dev \
-    python-qt4 python-qt4-dev python-qt4-sql python-sip python-sip-dev \
-    spawn-fcgi xauth xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xvfb 
+    python-qt4 python-qt4-dev python-qt4-sql python-sip python-sip-dev python-setuptools \
+    spawn-fcgi xauth xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xvfb \
+    python-pip gdal-bin python-gdal pyqt4.qsci-dev python-qscintilla2 python-pyspatialite
+
+pip install requests psycopg2 jinja2 pygments
 
 chmod -R a+w /usr/lib/x86_64-linux-gnu/qt4/plugins/designer/
 chmod -R a+w /usr/lib/python2.7/dist-packages/PyQt4/uic/widget-plugins/
