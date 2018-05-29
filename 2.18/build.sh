cd2e7d2fbd60ad8464b2c6193b8ed4264a85dfbe
Tim Sutton <tim@kartoza.com>
2018-05-29 09:29:44 +0200

    Added LTR tag by default for 2.18 builds. Updated LTR to 2.18.20

--- 2.18/build.sh
+++ 2.18/build.sh
@@ -7,7 +7,6 @@ source globals.sh
 ## Build
 ##
 docker build -t kartoza/qgis-desktop:${MAJOR}.${MINOR}.${BUGFIX} .
-docker build -t kartoza/qgis-desktop:LTR .
 sudo cp run-qgis-${MAJOR}.${MINOR}ltr-in-docker.sh /usr/local/bin
 sudo cp QGIS-${MAJOR}.${MINOR}LTR.Docker.desktop /usr/share/applications/
 sudo cp qgis-icon-60x60.png /usr/local
@@ -16,8 +15,10 @@ echo "A shortcut was created to run your docker - "
 echo "you may need to reload your desktop"
 echo "or log out and in again for the shortcut to be available."
 
+docker tag kartoza/qgis-desktop:${MAJOR}.${MINOR}.${BUGFIX} kartoza/qgis-desktop:LTR
 docker tag kartoza/qgis-desktop:${MAJOR}.${MINOR}.${BUGFIX} kartoza/qgis-desktop:${MAJOR}.${MINOR}
-docker tag kartoza/qgis-desktop:${MAJOR}.${MINOR}.${BUGFIX} kartoza/qgis-desktop:latest
+# We should rather push the 3.x image as latest
+#docker tag kartoza/qgis-desktop:${MAJOR}.${MINOR}.${BUGFIX} kartoza/qgis-desktop:latest
 docker push kartoza/qgis-desktop:${MAJOR}.${MINOR}.${BUGFIX}
 docker push kartoza/qgis-desktop:${MAJOR}.${MINOR}
 # We should rather push the 3.x image as latest
