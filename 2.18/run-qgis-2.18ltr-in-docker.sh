a5c9273ed5785c6fb2bb9fa481f4cd7bdb7b020b
Samweli <smwltwesa6@gmail.com>
2024-11-09 12:40:18 +0300

    Revert "support for 3.22"
    This reverts commit d2138a88f78d0fb2eb7f4ceaa3b5c37e08aac8ae.

--- 2.18/run-qgis-2.18ltr-in-docker.sh
+++ 2.18/run-qgis-2.18ltr-in-docker.sh
@@ -5,5 +5,5 @@ docker run --rm --name="qgis-desktop-ltr" \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=unix$DISPLAY \
 	--net=host \
-	kartoza/qgis-desktop:2.18
+	kartoza/qgis-desktop:LTR
 xhost -
--- 3.22/build/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-apt-get clean
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-rm -rf /build
-
-# Clean some stuff that are not needed by the server
-rm -r /usr/share/qgis/i18n
